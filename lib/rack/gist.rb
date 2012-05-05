d275218fa51503f96eb4a451dbf5b87df60db556
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2012-05-05 00:55:30 -0600

    upgrade default jquery

--- lib/rack/gist.rb
+++ lib/rack/gist.rb
@@ -118,7 +118,7 @@ module Rack
     end
 
     def jquery_link
-      "<script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js'></script>\n"
+      "<script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js'></script>\n"
     end
 
     def jquery_helper
